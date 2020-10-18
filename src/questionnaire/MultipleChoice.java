68530073a5c9d2678714c77ca9aab8829d991c4e
Chute, Lillith <lillithchute@maine.rr.com>
2020-10-18 14:44:16 -0400

    Added class to handle the multiple choice type of question.

--- src/questionnaire/MultipleChoice.java
+++ src/questionnaire/MultipleChoice.java
@@ -4,18 +4,18 @@ import java.util.Collections;
 import java.util.HashSet;
 
 /**
- * This kind of question is created via the text of the question, the correct answer, as well as
- * the options to select.
+ * This type of question gives a person multiple options to choose from only one of which is
+ * actually correct.
  */
-public class MultipleSelect implements Question {
+public class MultipleChoice implements Question {
 
   private final String question;
-  private final HashSet<String> answer = new HashSet<>();
+  private final String answer;
   private final HashSet<String> options = new HashSet<>();
   private static final HashSet<String> viable = new HashSet<>();
 
   /**
-   * Construct a MultipleSelect instance.
+   * Construct a MultipleChoice instance.
    *
    * @param questionText the question.
    * @param answer the correct answer which is passed in.
@@ -23,7 +23,7 @@ public class MultipleSelect implements Question {
    * @throws IllegalArgumentException if the question, answer, or options are missing
    *                                  , or the correct answers or options are invalid.
    */
-  public MultipleSelect(String questionText, String answer, String... options) {
+  public MultipleChoice(String questionText, String answer, String... options) {
     // Any of the passed in values can't be missing.
     if (questionText == null || questionText.length() == 0
             || answer == null || answer.length() == 0
@@ -54,26 +54,14 @@ public class MultipleSelect implements Question {
       throw new IllegalArgumentException("Duplicate options are not allowed.");
     }
 
-    Collections.addAll(this.answer, answer.split(" "));
-
-    // missing an answer.
-    if (this.answer.size() == 0) {
-      throw new IllegalArgumentException("An answer must be provided.");
+    // Make sure we have a viable correct answer.
+    if (!viable.contains(answer)) {
+      throw new IllegalArgumentException("No valid correct answer.");
     }
 
-    // Check for a correct answer that is available.
-    for (String item : this.answer) {
-      if (!viable.contains(item)) {
-        throw new IllegalArgumentException("No valid correct answer.");
-      }
-    }
-
-    // No duplicate answers allowed.
-    if (this.answer.size() != answer.split(" ").length) {
-      throw new IllegalArgumentException("Correct answers must be unique.");
-    }
-
+    // Good to go.
     this.question = questionText;
+    this.answer = answer;
   }
 
   @Override
@@ -83,54 +71,27 @@ public class MultipleSelect implements Question {
 
   @Override
   public String answer(String answer) {
-    // No answer provided then you are wrong.
-    if (answer == null || answer.length() == 0) {
-      return "Incorrect";
+    if (this.answer.equals(answer)) {
+      return "Correct";
     }
-
-    // Create a collection of answers.
-    HashSet<String> answerHash = new HashSet<>();
-    Collections.addAll(answerHash, answer.split(" "));
-
-    // If the number of correct answers do not match, you are wrong.
-    if (answerHash.size() != answer.split(" ").length) {
-      return "Incorrect";
-    }
-
-    // If the number of correct answers do not match, you are wrong.
-    if (this.answer.size() != answerHash.size()) {
-      return "Incorrect";
-    }
-
-
-    // If the answer chosen is not correct, then obviously you are wrong.
-    for (String correctAnswer : this.answer) {
-      if (!answerHash.contains(correctAnswer)) {
-        return "Incorrect";
-      }
-    }
-
-    // Otherwise, good job!
-    return "Correct";
+    return "Incorrect";
   }
 
   @Override
   public int compareTo(Question o) {
     if (o instanceof TrueFalse) {
       return 1;
-    } else if (o instanceof MultipleChoice) {
-      return 1;
     } else if (o instanceof Likert) {
       return -1;
+    } else if (o instanceof MultipleSelect) {
+      return -1;
     }
-
-    MultipleSelect question = (MultipleSelect) (o);
-
+    MultipleChoice question = (MultipleChoice) (o);
     return this.getText().compareTo(question.getText());
   }
 
   @Override
   public String toString() {
-    return "Multiple select question";
+    return "Multiple choice question";
   }
 }
